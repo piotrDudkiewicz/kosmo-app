f33cdc422f4c27f54848a3035aa558e6d69a895e
piter4d <pidudkiewicz632@gmail.com>
2020-07-07 00:17:12 +0200

    Final 1.0

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapperImage"></div>
+</template>
+
+<script>
+export default {
+  name: 'BackgroundImage',
+};
+</script>
+
+<style scoped>
+.wrapperImage{
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    z-index: -1;
+    top:0;
+    background-image: url("../assets/background.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: 70% 45%;
+}
+</style>
